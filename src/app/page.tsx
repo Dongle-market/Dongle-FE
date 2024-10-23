49b804e32ea49c7004a7d046fa1a329ec6f0c992
jiyounglee <skfnxh1021@naver.com>
2024-10-23 17:29:54 +0900

    Test Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,10 @@
-
 export default function Home() {
   return (
     <div className="page">
-      <main className="main">
-       
-        테스트 페이지 입니다!!!
+      <main className="main">테스트 페이지 입니다!!!</main>
+      <footer className="footer">푸터영역입니다.</footer>
 
-      </main>
-      <footer className="footer">
-        푸터영역입니다.
-      </footer>
+      <footer>Hello</footer>
     </div>
   );
 }
