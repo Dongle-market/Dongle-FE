a48b00c2d7d2ce45d5b5ed042538632f16a94eb3
jiyounglee <skfnxh1021@naver.com>
2024-10-24 13:09:46 +0900

    Last Testing Commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="page">
       <MainHeader />
-      <main className="main">소스트리 테스트합니다 감사합니다!!!</main>
+      <main className="main">마지막.. 소스트리 테스트합니다 양해 감사합니다..</main>
       <footer className="footer">푸터영역입니다.</footer>
       <FooterNav />
     </div>
