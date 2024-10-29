eb24551870eb036c6e0b604a8b1d634d72ba2571
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-10-29 16:42:15 +0900

    feat: 상품상세조회 상품이미지 추가

--- src/pages/item/index.tsx
+++ src/pages/item/index.tsx
@@ -7,8 +7,22 @@ import FooterNav from "@/components/navbar/ItemFooter";
 import styled from 'styled-components';
 import Link from "next/link";
 
+const ImageWrapper = styled.div`
+    position: relative;
+    width: 100%;
+    height: 390px;
+    border-radius: 4px;
+    overflow: hidden;
+`;
 
-
+const Image = styled.img`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+`;
 
 export default function Home() {
 
@@ -16,12 +30,19 @@ export default function Home() {
         "https://via.placeholder.com/24x24.png?text=%20",
         "https://via.placeholder.com/24x24.png?text=%20",
         "https://via.placeholder.com/24x24.png?text=%20",
-        // 추가 프로필 이미지 경로를 배열에 추가
       ];
 
     return (
         <div className="page">
             <Header />
+            <div className="content">
+                <ImageWrapper>
+                    <Image
+                        src="https://shopping-phinf.pstatic.net/main_1456236/14562361991.20240903141927.jpg"
+                        alt="Product Image"
+                    />
+                </ImageWrapper>
+            </div>
             <FooterNav price={44900} profileImages={profileImages} />
         </div>
     );
