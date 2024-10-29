a642ccc7f058886cee105bf793c4f9ded12239ac
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-10-29 17:16:24 +0900

    feat: 상품상세 상품 정보 텍스트 컴포넌트 구현

---
+++ src/components/items/ItemInfo.tsx
@@ -0,0 +1,74 @@
+// components/ItemInfo.tsx
+
+import styled from 'styled-components';
+import ForwardSvg from "/public/svgs/element/forward_arrow.svg";
+
+const InfoContainer = styled.div`
+    margin-top: 16px;
+    padding: 16px;
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+`;
+
+const CategoryContainer = styled.div`
+    font-size: 16px;
+    color: #919191;
+    display: flex;
+    align-items: center;
+`;
+
+const CategoryItem = styled.span`
+    display: flex;
+    align-items: center;
+    &:not(:last-child) {
+        margin-right: 2px; /* 각 카테고리와 다음 요소 사이에 2px 간격 설정 */
+    }
+`;
+
+const StyledArrow = styled(ForwardSvg)`
+    margin-left: 2px; /* 화살표와 다음 카테고리 사이에 2px 간격 설정 */
+`;
+
+const Brand = styled.div`
+    font-size: 16px;
+    color: #919191;
+    font-weight: 600;
+`;
+
+const ProductName = styled.div`
+    font-size: 20px;
+    font-weight: bold;
+    color: #000000;
+`;
+
+const PriceContainer = styled.div`
+    font-size: 24px;
+    font-weight: bold;
+    color: #000000;
+`;
+
+interface InfoSectionProps {
+    categories: string[];
+    brand: string;
+    productName: string;
+    price: number;
+}
+
+const ItemInfo = ({ categories, brand, productName, price }: InfoSectionProps) => (
+    <InfoContainer>
+        <CategoryContainer>
+            {categories.map((category, index) => (
+                <CategoryItem key={index}>
+                    {category}
+                    {index < categories.length - 1 && <StyledArrow />}
+                </CategoryItem>
+            ))}
+        </CategoryContainer>
+        <Brand>{brand}</Brand>
+        <ProductName>{productName}</ProductName>
+        <PriceContainer>{price.toLocaleString('ko-KR')} 원</PriceContainer>
+    </InfoContainer>
+);
+
+export default ItemInfo;
