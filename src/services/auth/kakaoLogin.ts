ae8a96608818fff58e4d0b62eab59dc93f7ac0dd
seungboshim <fodo9898@gmail.com>
2024-10-30 17:20:32 +0900

    feat: PC, 모바일 감지 함수

--- src/services/auth/kakaoLogin.ts
+++ src/services/auth/kakaoLogin.ts
@@ -1,5 +1,12 @@
+import { isMobile } from "@/utils/detectDevice"
+
 export const kakaoLogin = () => {
-  window.Kakao.Auth.authorize({
-    redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
-  })
+  if (isMobile()) {
+    window.Kakao.Auth.authorize({
+      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+    })
+  } else {
+    console.log("PC 환경에서는 지원하지 않습니다.")
+  }
+
 }
