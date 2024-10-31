aecaf5c58a86296f214b26223c0b318b60309c43
seungboshim <fodo9898@gmail.com>
2024-10-31 14:55:19 +0900

    feat: 서버 포트변경 대응

--- src/services/auth/kakao.ts
+++ src/services/auth/kakao.ts
@@ -1,12 +1,14 @@
 import { isMobile } from "@/utils/detectDevice"
 
 export const kakaoLogin = async () => {
+  const redirectUri = process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI || 'http://localhost:3000/auth/callback';
+  console.log(redirectUri)
   if (isMobile()) {
     window.Kakao.Auth.authorize({
-      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+      redirectUri: redirectUri,
     })
   } else {
-    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI}&response_type=code`;
+    const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.NEXT_PUBLIC_KAKAO_CLIENT_ID}&redirect_uri=${redirectUri}&response_type=code`;
     window.location.href = kakaoAuthUrl;
   }
 }
--- src/services/axios.setting.ts
+++ src/services/axios.setting.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const Server = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_SERVER_LOCAL,
+  baseURL: process.env.NEXT_PUBLIC_SERVER || 'http://localhost:4000/apis',
   headers: {
     'Content-Type': 'application/json',
   },
