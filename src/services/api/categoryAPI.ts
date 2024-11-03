ec36c8722e0fc0d9c52173f6be58aa49a78a2b5d
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-03 15:28:49 +0900

    feat: api fetch Service.get 으로 수정

--- src/services/api/categoryAPI.ts
+++ src/services/api/categoryAPI.ts
@@ -1,9 +1,14 @@
 // /services/categoryAPI.ts
 
+import { Server } from '../axios.setting';
+
 export async function fetchCategoryData(species: string, sub: string) {
-    const response = await fetch(`http://54.180.249.135:4000/apis/item/food?species=${species}&sub=${sub}`);
-    if (!response.ok) {
-        throw new Error("Failed to fetch category data");
+    try {
+      const response = await Server.get('/item/food', {
+        params: { species, sub },
+      });
+      return response.data; // 데이터만 반환
+    } catch (error) {
+      throw new Error("Failed to fetch category data");
     }
-    return response.json();
-}
+  }
