fd2a710e595fc029bbf5ea5a74adb6308c7e0a3a
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-03 16:54:49 +0900

    feat: 요청 API에 order 쿼리 추가

--- src/services/api/categoryAPI.ts
+++ src/services/api/categoryAPI.ts
@@ -2,13 +2,13 @@
 
 import { Server } from '../axios.setting';
 
-export async function fetchCategoryData(species: string, sub: string) {
+export async function fetchCategoryData(species: string, sub: string, order: string) {
     try {
       const response = await Server.get('/item/food', {
-        params: { species, sub },
+        params: { species, sub, order },
       });
       return response.data; // 데이터만 반환
     } catch (error) {
       throw new Error("Failed to fetch category data");
     }
-  }
+}
