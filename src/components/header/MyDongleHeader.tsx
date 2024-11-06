7286db407d0513fa7192bbde5a7fc5a4659e5213
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-07 01:40:18 +0900

    feat: 마이동글 페이지 헤더 조회에 맞게 수정

--- src/components/header/MyDongleHeader.tsx
+++ src/components/header/MyDongleHeader.tsx
@@ -1,5 +1,4 @@
 // MyDongleHeader.tsx
-"use client";
 
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
@@ -7,6 +6,9 @@ import Image from "next/image";
 import AddPetSvg from "../../../public/svgs/pet/addpet.svg";
 import AddPetActiveSvg from "../../../public/svgs/pet/addpetactive.svg";
 import { useRouter } from "next/router";
+import { getPets } from "@/services/pets/pets";
+import { imageMap } from "../items/PetsPort";
+import { useParams } from "next/navigation";
 
 const PetHeader = styled.div`
   padding-top: 36px;
@@ -37,20 +39,54 @@ const AddPetIcon = styled.div`
   cursor: pointer;
 `;
 
-const petProfiles = [
-  { id: 1, src: "/images/selectpets/DogEmoji.png", alt: "Dog" },
-  { id: 2, src: "/images/selectpets/CatEmoji.png", alt: "Cat" }
+interface PetType {
+  petId: number;
+  profileImg: string;
+  petName: string;
+}
+
+const profiles = [
+  { id: 1, imageurl: "/images/petprofileimages/dog1.png" },
+  { id: 2, imageurl: "/images/petprofileimages/cat1.png" },
+  { id: 3, imageurl: "/images/petprofileimages/dog2.png" },
+  { id: 4, imageurl: "/images/petprofileimages/cat2.png" },
+  { id: 5, imageurl: "/images/petprofileimages/dog3.png" },
+  { id: 6, imageurl: "/images/petprofileimages/cat3.png" },
 ];
+
 
 const MyDongleHeader = () => {
   const router = useRouter();
   const [isActive, setIsActive] = useState(false);
   const [selectedProfile, setSelectedProfile] = useState<number | null>(null);
+  const [petProfiles, setPetProfiles] = useState<PetType[]>([]);
 
   useEffect(() => {
     if (router.pathname === '/mydongle/add') {
       setIsActive(true);
     }
+
+    const urlPetId = Number(router.asPath.split("/").pop());
+    if (!isNaN(urlPetId)) {
+      setSelectedProfile(urlPetId);
+    }
+
+    // API를 호출하여 유저의 반려동물 데이터를 가져오기
+    const fetchPets = async () => {
+      try {
+        const pets = await getPets();
+        const formattedPets = pets.map((pet) => ({
+          petId: pet.petId,
+          profileImg: imageMap[pet.profileImg] || "", // profileImg 파일 경로 지정
+          petName: pet.petName,
+        }));
+        setPetProfiles(formattedPets);
+      } catch (error) {
+        console.error("반려동물 데이터를 불러오는 데 실패했습니다.", error);
+      }
+    };
+
+    fetchPets();
   }, [router.pathname]);
 
   const handleAddPetClick = () => {
@@ -59,27 +95,26 @@ const MyDongleHeader = () => {
     }
   };
 
-  const handleProfileClick = (id: number) => {
-    if (router.pathname !== '/mydongle/add') {
-      setSelectedProfile(id);
-    }
+  const handleProfileClick = (petId: number) => {
+    router.push(`/mydongle/${petId}`);
+    setSelectedProfile(petId);
   };
 
   return (
     <PetHeader>
-      {petProfiles.map((profile) => (
-        <PetProfile
-          key={profile.id}
-          selected={selectedProfile === profile.id}
-          onClick={() => handleProfileClick(profile.id)}
-        >
-          <Image src={profile.src} alt={profile.alt} layout="fill" objectFit="cover" />
-        </PetProfile>
-      ))}
-      <AddPetIcon onClick={handleAddPetClick}>
-        {isActive ? <AddPetActiveSvg /> : <AddPetSvg />}
-      </AddPetIcon>
-    </PetHeader>
+    {petProfiles.map((profile) => (
+      <PetProfile
+        key={profile.petId}
+        selected={selectedProfile === profile.petId}
+        onClick={() => handleProfileClick(profile.petId)}
+      >
+        <Image src={profile.profileImg} alt={profile.petName} layout="fill" objectFit="cover" />
+      </PetProfile>
+    ))}
+    <AddPetIcon onClick={handleAddPetClick}>
+      {isActive ? <AddPetActiveSvg /> : <AddPetSvg />}
+    </AddPetIcon>
+  </PetHeader>
   );
 };
 
