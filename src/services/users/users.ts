b6cd5742efb42d60bb9573688a693533ca8e4a41
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-06 17:31:30 +0900

    feat: user 수정 api 추가

--- src/services/users/users.ts
+++ src/services/users/users.ts
@@ -10,3 +10,13 @@ export const getUserInfo = async (): Promise<UserResponse> => {
     throw new Error();
   }
 }
+
+export const updateUserInfo = async (updateData: Partial<UserResponse>): Promise<UserResponse> => {
+  try {
+    const response = await Server.patch('/user', updateData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw new Error();
+  }
+};
