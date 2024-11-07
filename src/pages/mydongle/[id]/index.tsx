03522a2963643aca479a192f478a08a79ec1bd08
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-07 10:15:22 +0900

    hotfix: 빌드 오류 해결

--- src/pages/mydongle/[id]/index.tsx
+++ src/pages/mydongle/[id]/index.tsx
@@ -5,85 +5,14 @@ import styled from "styled-components";
 import MyDongleFooterNav from "@/components/navbar/MyDongleFooterNav";
 import Header from "@/components/header/CategoryHeader";
 import MyDongleHeader from "@/components/header/MyDongleHeader";
-import CategoryItem from "@/components/items/CategoryItem";
 import MyDongleHistoryItem from "@/components/items/MyDongleHistoryItem";
 import PetsPort from "@/components/items/PetsPort";
 import { PetInfoResponseType } from "@/services/pets/pets.type";
 import { getPetInfo } from "@/services/pets/pets";
-import { useRouter } from "next/router";
 import { useParams } from "next/navigation";
 
-interface WishlistItem {
-  itemId: number;
-  image: string;
-  title: string;
-  lprice: number;
-  selectedPetIds?: number[];
-}
 
-const items: WishlistItem[] = [
-  { itemId: 1, image: "/images/An.png", title: "보류입니다.", lprice: 34000 },
-  { itemId: 2, image: "/images/Baek.png", title: "어얼얽--", lprice: 34000 },
-  {
-    itemId: 3,
-    image: "/images/An.png",
-    title: "고기가 이븐하게 익지 않아써여",
-    lprice: 34000,
-  },
-  {
-    itemId: 4,
-    image: "/images/Son&Jeon.png",
-    title: "왜저뤠ㅞㅞㅞ~~",
-    lprice: 34000,
-  },
-  {
-    itemId: 5,
-    image: "/images/An.png",
-    title: "저는 채소의 익힘 정도를 굉장히 중요시 여기거덩여",
-    lprice: 34000,
-  },
-  {
-    itemId: 6,
-    image: "/images/Baek.png",
-    title: "이거 빠쓰자나~ 어허~ 재밌네 이거ㅎㅎ",
-    lprice: 34000,
-  },
-  {
-    itemId: 7,
-    image: "/images/product1.png",
-    title: "도치빌 리더스",
-    lprice: 34000,
-  },
-  {
-    itemId: 8,
-    image: "/images/product1.png",
-    title: "도치빌 리더스",
-    lprice: 34000,
-  },
-  {
-    itemId: 9,
-    image: "/images/product1.png",
-    title: "도치빌 리더스",
-    lprice: 34000,
-  },
-  {
-    itemId: 10,
-    image: "/images/product1.png",
-    title: "도치빌 리더스",
-    lprice: 34000,
-  },
-];
-
-interface MyDongleHistoryItemProps {
-  itemId: number;
-  image: string;
-  title: string;
-  lprice: number;
-  date: string;
-  selectedPetIds?: number[];
-}
-
-const initialHistoryItems: MyDongleHistoryItemProps[] = [
+const initialHistoryItems = [
   {
     itemId: 1,
     image: "/images/An.png",
@@ -162,97 +91,6 @@ const initialHistoryItems: MyDongleHistoryItemProps[] = [
     lprice: 34000,
     date: "2024-11-04",
     selectedPetIds: [1, 2, 3],
-  },
-];
-
-const initialCartItems = [
-  {
-    id: 1,
-    imageurl: "/images/Son&Jeon.png",
-    brand: "아디다스",
-    name: "왜저뤠ㅞㅞ~~",
-    price: 34000,
-    selected: true,
-  },
-  {
-    id: 2,
-    imageurl: "/images/Baek.png",
-    brand: "아디다스",
-    name: "어얼얽--",
-    price: 34000,
-    selected: true,
-  },
-  {
-    id: 3,
-    imageurl: "/images/An.png",
-    brand: "아디다스",
-    name: "고기가 이븐하게 익지 않아써여",
-    price: 34000,
-    selected: true,
-  },
-  {
-    id: 4,
-    imageurl: "/images/An.png",
-    brand: "아디다스",
-    name: "보류입니다.",
-    price: 34000,
-    selected: true,
-  },
-  {
-    id: 5,
-    imageurl: "/images/An.png",
-    brand: "아디다스",
-    name: "저는 채소의 익힘 정도를 굉장히 중요시 여기거덩여",
-    price: 34000,
-    selected: true,
-  },
-  {
-    id: 6,
-    imageurl: "/images/Baek.png",
-    brand: "아디다스",
-    name: "이거 빠쓰자나~ 어허~ 재밌네 이거ㅎㅎ",
-    price: 34000,
-    selected: true,
-  },
-  {
-    id: 7,
-    imageurl: "/images/product1.png",
-    brand: "아디다스",
-    name: "도치빌 리더스",
-    price: 34000,
-    selected: false,
-  },
-  {
-    id: 8,
-    imageurl: "/images/product1.png",
-    brand: "아디다스",
-    name: "도치빌 리더스",
-    price: 34000,
-    selected: false,
-  },
-  {
-    id: 9,
-    imageurl: "/images/product1.png",
-    brand: "아디다스",
-    name: "도치빌 리더스",
-    price: 34000,
-    selected: false,
-  },
-  {
-    id: 10,
-    imageurl: "/images/product1.png",
-    brand: "아디다스",
-    name: "도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스도치빌 리더스",
-    price: 34000,
-    selected: false,
-  },
-  {
-    id: 11,
-    imageurl: "/images/Son&Jeon.png",
-    brand: "아디다스",
-    name: "왜저뤠ㅞㅞ~~",
-    price: 34000,
-    selected: false,
   },
 ];
 
@@ -347,7 +185,6 @@ export default function MyDonglePage() {
   const [activeTab, setActiveTab] = useState(0);
   const [items, setItems] = useState(initialHistoryItems);
   const [, setSelectedItems] = useState<SelectedItems>({});
-  const [cartItems] = useState(initialCartItems);
   const [petData, setPetData] = useState<PetInfoResponseType>();
 
   useEffect(() => {
